ab4400241d4e9dc1686c83c5e293cdcee7ffbbf0
JusupJoseph <busurmankulovzusup@gmail.com>
2023-01-13 17:58:13 +0600

    Build Clean Architecture

---
+++ app/src/main/java/com/example/projectteam/presentation/ui/activity/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.projectteam.presentation.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.projectteam.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
